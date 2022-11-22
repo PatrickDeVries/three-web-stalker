c8d418b9bb0391a7a020814587b982ff8aaa3d19
Patrick DeVries <pdevries@headstorm.com>
2022-11-21 22:54:47 -0600

    Initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  jsxSingleQuote: false,
+  printWidth: 100,
+  quoteProps: 'as-needed',
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-organize-imports'],
+}
